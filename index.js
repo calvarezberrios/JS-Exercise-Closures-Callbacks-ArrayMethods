bd7085b5a7ee99a7ea606c6ba3d3e5b54e7b5ff3
Mannie <carlos.alvarezberrios@gmail.com>
2020-02-11 22:11:25 -0500

    getRunnersByTShirtSize() test successdul

--- index.js
+++ index.js
@@ -230,8 +230,13 @@ function firstNamesAllCaps(runners) {
  * @returns an array containing only the runners that use the given `tShirtSize`.
  * The runners in the array appear in the same order they appear in the `runners` array.
 */
-function getRunnersByTShirtSize(/* CODE HERE */) {
+function getRunnersByTShirtSize(runners, tShirtSize) {
   /* CODE HERE */
+  const runnersTShirtSize = runners.filter((sizes) => {
+    return sizes.shirt_size == tShirtSize;
+  });
+  
+  return runnersTShirtSize;
 }
 
 /**
